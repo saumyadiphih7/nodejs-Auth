4fa0d091e58d73e5c7a660911300085b29f7d691
saumyadiphih7 <saumyadip@hih7.com>
2025-01-11 16:57:16 +0530

    Modified the code

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,29 +1,28 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-
-const authMiddleware = (req, res, next) => { 
+const authMiddleware = (req, res, next) => {
   let token;
 
-  if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-    token = req.headers.authorization.split(' ')[1]
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
   }
 
-  if(!token){
-    res.status(401)
-    throw new Error('Not authorized, no token')
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
   }
-    
-    try {
-      
-      const decoded = jwt.verify(token, process.env.JWT_SECRET)
-      req.user = decoded
-      next()
-    } catch (error) {
-      res.status(401)
-      throw new Error('Not authorized, token failed')
-    }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401);
+    throw new Error("Not authorized, token failed");
   }
- 
+};
+
 module.exports = authMiddleware;
-  
-
