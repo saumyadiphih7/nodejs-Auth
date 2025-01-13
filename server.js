ba23b1a0c0b5d8343a5ba8e13bc29aaf5690ac07
saumyadiphih7 <saumyadip@hih7.com>
2025-01-13 16:47:35 +0530

    some changes added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require("dotenv")
 const connectDB = require('./config/db')
-const errorHandler = require('./middlewares/errorHandler')
+const errorHandler = require('./middlewares/errorHandler.js')
 
 dotenv.config()
 
